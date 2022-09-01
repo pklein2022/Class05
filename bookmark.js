7536526a4edbf973f712d9b77b742b9cba9e3292
pklein2022 <111322242+pklein2022@users.noreply.github.com>
2022-08-31 21:42:49 -0500

    Bookmark initial work

---
+++ bookmark.js
@@ -0,0 +1,18 @@
+const body = document.body;
+const input = document.querySelector('input[type=text]');
+const overlay = document.querySelector('.overlay');
+
+function showFloater(){
+    body.classList.add('show-floater');
+}
+function closeFloater(){
+    if(body.classList.contains('show-floater')){
+        body.classList.remove('show-floater');
+    }
+
+}
+
+input.addEventListener('focusin', showFloater);
+input.addEventListener('focusout', closeFloater);
+
+overlay.addEventListener('click', closeFloater);
